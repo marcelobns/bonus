1b81fdc7b4a21aaf7b5f730b721f213ba4ac3549
Marcelo Bonus <marcelobns@live.com>
2015-10-31 12:20:10 -0400

    slug rewrited
    based in regex

--- src/View.php
+++ src/View.php
@@ -169,33 +169,21 @@ class View {
 		return $buffer;
 	}
 	
-	function slug($string) {
-		$string = strtolower($string);
-		// Código ASCII das vogais
-		$ascii['a'] = range(224, 230);
-		$ascii['e'] = range(232, 235);
-		$ascii['i'] = range(236, 239);
-		$ascii['o'] = array_merge(range(242, 246), array(240, 248));
-		$ascii['u'] = range(249, 252);
-		// Código ASCII dos outros caracteres
-		$ascii['b'] = array(223);
-		$ascii['c'] = array(231);
-		$ascii['d'] = array(208);
-		$ascii['n'] = array(241);
-		$ascii['y'] = array(253, 255);
-		foreach ($ascii as $key=>$item) {
-			$acentos = '';
-			foreach ($item AS $codigo) $acentos .= chr($codigo);
-			$troca[$key] = '/['.$acentos.']/i';
-		}
-		$string = preg_replace(array_values($troca), array_keys($troca), $string);
-		
-		// Troca tudo que não for letra ou número por um caractere ($slug)
-		$string = preg_replace('/[^a-z0-9]/i', $slug, $string);
-		// Tira os caracteres ($slug) repetidos
-		$string = preg_replace('/' . $slug . '{2,}/i', $slug, $string);
-		$string = trim($string, $slug);
-		
-		return $string;
+	function slugify($text){
+	  // replace non letter or digits by -
+	  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+	  // trim
+	  $text = trim($text, '-');
+	  // transliterate
+	  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+	  // lowercase
+	  $text = strtolower($text);
+	  // remove unwanted characters
+	  $text = preg_replace('~[^-\w]+~', '', $text);
+
+	  if (empty($text)){
+	    return 'n-a';
+	  }
+	  return $text;
 	}
 }
