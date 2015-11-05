ff0d4ae1319407c23e71af3108e2ccac3fcdbfb3
Marcelo Bonus <marcelobns@live.com>
2015-11-05 14:10:11 -0400

    prioridade nas permissions do user

--- src/Permission.php
+++ src/Permission.php
@@ -9,19 +9,20 @@ require_once(SRC."routes.php");
 
 class Permission {
     public static function check($url){
-        $routes = Routes::guest();
-        foreach ($routes as $route => $array) {
-			if(@preg_match($route, $url) || $route == $url){
-				return $routes[$route];
-			}
-		}        
-        if(isset($_SESSION['permissions']) && sizeof($_SESSION['permissions']) > 0){
+    	if(isset($_SESSION['permissions']) && sizeof($_SESSION['permissions']) > 0){
             foreach ($_SESSION['permissions'] as $i => $value) {
                 if(@preg_match($value['route'], $url) || $value['route'] == $url){
-    				return $value['allow'];
-    			}
+			return $value['allow'];
+		}
             }
         }
+    	
+        $routes = Routes::guest();
+        foreach ($routes as $route => $array) {
+		if(@preg_match($route, $url) || $route == $url){
+			return $routes[$route];
+		}
+	}        
         return false;
     }
 }
